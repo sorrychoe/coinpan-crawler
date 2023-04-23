39af41f005497994bf619cdfc4c53d888f829d66
sorrychoe <cjssoote@gmail.com>
2023-04-23 22:38:34 +0900

    modified

--- coinpan_crawler.py
+++ coinpan_crawler.py
@@ -1,7 +1,6 @@
 import re
 import time
 
-import chromedriver_autoinstaller
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,21 +8,13 @@ import streamlit as st
 from konlpy.tag import Okt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from wordcloud import WordCloud
 
 # set font & size
-plt.rcParams["font.family"] = "Apple Gothic"
+plt.rcParams["font.family"] = "Hancom MalangMalang"
 plt.rcParams["figure.figsize"] = 10, 10
 okt = Okt()
-
-
-def word_counter(value, key_words):
-    for i in value:
-        if i not in key_words:
-            key_words[i] = 1
-        elif i in key_words:
-            key_words[i] += 1
-    return key_words
 
 
 def tokenizer(text):
@@ -35,53 +26,35 @@ def tokenizer(text):
     return lis
 
 
-def no_space(text):
-    a = re.sub("\n", "", text)
-    b = re.sub("  ", "", a)
-    c = re.sub("[0-9]+ ", "", b)
-    return c
-
-
-def counter_to_DataFrame(key_words):
-    word_df = pd.DataFrame(key_words.items())
-    word_df.columns = ["단어", "빈도"]
-    word_df = word_df.sort_values(["빈도"], ascending=False).reset_index(drop=True)  # 내림차순 정렬
-    return word_df
-
-
-def wordcloud(key_words):
-    df = counter_to_DataFrame(key_words)
-    df = df.drop([0, 1, 2])  # 불용어 제거
-    df = df[df["단어"].str.len() > 1]
-    df.reset_index(drop=True, inplace=True)
-
-    wc = WordCloud(font_path="Apple Gothic", width=500, height=500, background_color="white").generate_from_frequencies(
-        df.set_index("단어").to_dict()["빈도"]
-    )
-    return wc
+def parsing_keywords(text):
+    no_space = re.sub("\n", "", text)
+    no_enter = re.sub("  ", "", no_space)
+    no_nbsp = re.sub("&nbsp;", "", no_enter)
+    return no_nbsp
 
 
 def main():
     st.header("코인판 키워드 분석기")
     st.subheader("페이지가 너무 많을 경우, 속도가 느려질 수 있습니다.")
     iter = st.number_input("몇 페이지까지의 키워드를 확인하겠습니까?: ", 1, 100)
-
-    key_words = {}
+    time.sleep(10)
 
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
-    # options.add_argument("--window-size=1920x1080")
-    # options.add_argument("--disble-gpu")
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disble-gpu")
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     driver.implicitly_wait(5)
 
+    iter = 1
+
     url = "https://coinpan.com/index.php?mid=free&page=1"
     driver.get(url)
 
+    words = []
+    text_tag = "div.board_read.rd > div.section_wrap.section_border_0 > div > div"
     for i in range(3, iter + 3):
-
-        word_lis = []
 
         # 딜레이 생성
         seed = np.random.randint(100)
@@ -90,38 +63,46 @@ def main():
         time.sleep(a)
 
         for j in range(6, 26):
-            title = driver.find_element(
-                By.CSS_SELECTOR, f"table > tbody > tr:nth-child({j}) > td.title > a"
-            ).get_attribute("text")
-            title = no_space(title)
-            name = driver.find_element(
-                By.CSS_SELECTOR, f"table > tbody > tr:nth-child({j}) > td.author > a"
-            ).get_attribute("text")
+            title = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.title > a").get_attribute("text")
 
-            word_lis.append(title)
-        token = tokenizer(word_lis)
-        key_words = word_counter(token, key_words)
+            name = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.author > a").get_attribute("text")
+
+            date = driver.find_element(
+                By.CSS_SELECTOR, f"tr:nth-child({j}) > td.time > span.regdateHour"
+            ).get_attribute("innerText")
+
+            freq = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.readed > span.number").get_attribute(
+                "innerText"
+            )
+
+            driver.find_element(By.CSS_SELECTOR, f"table > tbody > tr:nth-child({j}) > td.title > a").click()
+
+            if text_checker(text_tag):
+                context = driver.find_element(By.CSS_SELECTOR, text_tag).get_attribute("text")
+            else:
+                context = " "
+            driver.back()
+
+            title = parsing_keywords(title)
+            # context = parsing_keywords(context)
+            words.append([title, name, context])
 
         if i <= 7:
-            driver.find_element(By.XPATH, f"/div[2]/div/ul/li[{i}]/a").click()
+            driver.find_element(By.CSS_SELECTOR, f"div.section_footer > div > ul > li:nth-child({i}) > a").click()
             time.sleep(3)
+
         else:
-            driver.find_element(By.XPATH, f"/div[2]/div/ul/li[8]/a").click()
+            driver.find_element(By.CSS_SELECTOR, f"div.section_footer > div > ul > li:nth-child(8) > a").click()
             time.sleep(3)
 
     driver.quit()
 
-    counter = counter_to_DataFrame(key_words)
-    counter = counter[counter["단어"].str.len() > 1]
-    counter.reset_index(drop=True, inplace=True)
+    df = pd.DataFrame(words, columns=["제목", "작성자", "업로드_일자", "조회수", "게시글_내용"]).drop_duplicates()
 
-    fig = plt.figure()
-    plt.title(str(iter) + "페이지까지의 " + "KeyWords")
-    wc = wordcloud(key_words)
-    plt.imshow(wc)
-    plt.axis("off")
-    plt.show()
-    st.pyplot(fig)
+    try:
+        driver.quit()
+    except WebDriverException:
+        st.download_button(label="파일을 다운로드해주세요.", data=data, file_name="coinpan.xlsx")
 
 
 if __name__ == "__main__":
