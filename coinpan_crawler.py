6a91dd5acb24a282f73253bc29f4c50745177f9e
sorrychoe <cjssoote@gmail.com>
2023-04-30 18:14:49 +0900

    modified

--- coinpan_crawler.py
+++ coinpan_crawler.py
@@ -8,19 +8,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# set font & size
-plt.rcParams["font.family"] = "Hancom MalangMalang"
-plt.rcParams["figure.figsize"] = 10, 10
-okt = Okt()
 
-
-def tokenizer(text):
-    lis = []
-    for i in text:
-        word = okt.nouns(i)
-        for k in word:
-            lis.append(k)
-    return lis
+def text_checker(tag, driver):
+    if driver.find_elements(By.CSS_SELECTOR, tag):
+        return True
+    else:
+        return False
 
 
 def parsing_keywords(text):
@@ -32,7 +25,6 @@ def parsing_keywords(text):
 
 def main():
     st.header("코인판 키워드 분석기")
-    st.subheader("페이지가 너무 많을 경우, 속도가 느려질 수 있습니다.")
     iter = st.number_input("몇 페이지까지의 키워드를 확인하겠습니까?: ", 1, 100)
     time.sleep(10)
 
@@ -50,59 +42,60 @@ def main():
     driver.get(url)
 
     words = []
-    text_tag = "div.board_read.rd > div.section_wrap.section_border_0 > div > div"
+    text_tag = "div.board_read.rd > div.section_wrap.section_border_0 > div > div > p"
+
     for i in range(3, iter + 3):
 
-        # 딜레이 생성
-        seed = np.random.randint(100)
-        np.random.seed(seed)
-        a = np.random.randint(5)
-        time.sleep(a)
-
         for j in range(6, 26):
-            title = driver.find_elements(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.title > a").get_attribute(
-                "text"
-            )
+            title = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.title > a").text
 
-            name = driver.find_elements(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.author > a").get_attribute(
-                "text"
-            )
+            name = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.author > a").text
 
-            date = driver.find_elements(
-                By.CSS_SELECTOR, f"tr:nth-child({j}) > td.time > span.regdateHour"
-            ).get_attribute("innerText")
+            date = driver.find_element(
+                By.CSS_SELECTOR, f"tr:nth-child({j}) > td.time > span.number > span.regdateHour"
+            ).text
 
-            freq = driver.find_elements(
-                By.CSS_SELECTOR, f"tr:nth-child({j}) > td.readed > span.number"
-            ).get_attribute("innerText")
+            freq = driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({j}) > td.readed > span.number").text
 
-            driver.find_elements(By.CSS_SELECTOR, f"table > tbody > tr:nth-child({j}) > td.title > a").click()
+            driver.find_element(By.CSS_SELECTOR, f"table > tbody > tr:nth-child({j}) > td.title > a").click()
 
-            if text_checker(text_tag):
-                context = driver.find_elements(By.CSS_SELECTOR, text_tag).get_attribute("text")
+            # delaying
+            seed = np.random.randint(100)
+            np.random.seed(seed)
+            a = np.random.randint(5)
+            time.sleep(a)
+
+            if text_checker(text_tag, driver):
+                ptags = driver.find_elements(By.CSS_SELECTOR, text_tag)
+                contexts = ""
+                for k in range(len(ptags)):
+                    texts = f"div.board_read.rd > div.section_wrap.section_border_0 > div > div > p:nth-child({k+1})"
+                    context = driver.find_element(By.CSS_SELECTOR, texts).text
+                    context = parsing_keywords(context)
+                    contexts = contexts + " " + context
+
             else:
-                context = " "
+                contexts = ""
             driver.back()
 
             title = parsing_keywords(title)
-            # context = parsing_keywords(context)
-            words.append([title, name, context])
+            words.append([title, name, date, freq, contexts])
 
         if i <= 7:
-            driver.find_elements(
+            driver.find_element(
                 By.CSS_SELECTOR, f"div.section_footer > div > ul > li:nth-child({i}) > a"
             ).click()
             time.sleep(3)
 
         else:
-            driver.find_elements(
+            driver.find_element(
                 By.CSS_SELECTOR, f"div.section_footer > div > ul > li:nth-child(8) > a"
             ).click()
             time.sleep(3)
 
     driver.quit()
 
-    df = pd.DataFrame(words, columns=["제목", "작성자", "업로드_일자", "조회수", "게시글_내용"]).drop_duplicates()
+    df = pd.DataFrame(words, columns=["제목", "작성자", "업로드_일자", "조회수", "게시글_내용"]).drop_duplicates(subset=["제목"])
 
     try:
         driver.quit()
